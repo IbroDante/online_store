7bc09505191c54bdb020597e16e1fc692e317997
ibrodante <ibroman02@yahoo.com>
2024-06-12 12:47:32 +0100

    Fix bugs: Resolve issues with nested serializers and data representation

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -5,14 +5,14 @@ from .models import Item, Supplier
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['id', 'name', 'description', 'price', 'date_added']
+        fields = ['id', 'name', 'description', 'price', 'date_added'] 
 
 class SupplierSerializer(serializers.ModelSerializer):
     items = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all(), many=True)
 
     class Meta:
         model = Supplier
-        fields = ['id', 'name', 'contact_information', 'items']
+        fields = ['id', 'name', 'contact_information', 'items'] 
 
 class SupplierDetailSerializer(serializers.ModelSerializer):
     items = ItemSerializer(many=True, read_only=True)
